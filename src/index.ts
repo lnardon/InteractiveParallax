0261d8144a047e26c65d1a0d4f3d7f8ca984cfeb
lnardon <lucaslnardon@gmail.com>
2021-04-12 02:19:39 -0300

    ✨ Add obj parallax function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export const useParallax = (target: string = "", offsetTop: number = 0) => {
   window.onload = () => {
+    addMouseListener();
     let elements = [...document.getElementsByClassName(target)];
     elements.forEach((element) => {
       if (
@@ -24,4 +25,21 @@ export const useParallax = (target: string = "", offsetTop: number = 0) => {
       });
     };
   };
+};
+
+const addMouseListener = () => {
+  document.addEventListener("mousemove", (e) => {
+    console.log(e);
+  });
+};
+
+const parallaxObjects = (className: string, e: MouseEvent) => {
+  const objs = [...document.querySelectorAll(`.${className}`)];
+  objs.forEach((elem) => {
+    const vel = Number(elem.getAttribute("parallax-speed"));
+    const x = window.innerWidth - e.pageX * vel;
+    const y = window.innerHeight - e.pageY * vel;
+
+    elem.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
 };
