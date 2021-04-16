90006f57164a9f2c6fa7f0103c2ae4208d2b9dee
lnardon <lucaslnardon@gmail.com>
2021-04-15 22:56:16 -0300

    ♻️ Refactor basic code for release

--- src/index.ts
+++ src/index.ts
@@ -3,47 +3,30 @@ export const useParallax = (
   offsetTop: number = 0
 ) => {
   window.onload = () => {
-    addMouseListener(targetClass);
-    let elements = [...document.getElementsByClassName(targetClass)];
-    elements.forEach((element) => {
-      if (
-        element.getBoundingClientRect().top < window.innerHeight - offsetTop &&
-        element.getBoundingClientRect().bottom
-      ) {
-        element.classList.add("slideUp");
-      }
-    });
-    window.onscroll = (e: Event) => {
-      let elements = [...document.getElementsByClassName(targetClass)];
-      elements.forEach((element) => {
-        if (
-          element.getBoundingClientRect().top <
-            window.innerHeight - offsetTop &&
-          element.getBoundingClientRect().bottom
-        ) {
-          element.classList.add("slideUp");
-        } else {
-          element.classList.remove("slideUp");
-        }
+    // Adds mouse listener
+    document.addEventListener("mousemove", (e) => {
+      const objs: any = [...document.getElementsByClassName(`.${targetClass}`)];
+      objs.forEach((elem: HTMLElement) => {
+        const vel = Number(elem.getAttribute("parallax-speed"));
+        const x = (window.innerWidth - e.pageX * vel) / 100;
+        const y = (window.innerHeight - e.pageY * vel) / 100;
+
+        elem.style.transform = `translateX(${x}px) translateY(${y}px)`;
       });
-    };
+    });
   };
 };
 
-const addMouseListener = (targetClass: string) => {
-  document.addEventListener("mousemove", (e) => {
-    console.log(e);
-    parallaxObjects(targetClass, e);
-  });
-};
-
-const parallaxObjects = (targetClass: string, e: MouseEvent) => {
-  const objs = [...document.querySelectorAll(`.${targetClass}`)];
-  objs.forEach((elem) => {
-    const vel = Number(elem.getAttribute("parallax-speed"));
-    const x = window.innerWidth - e.pageX * vel;
-    const y = window.innerHeight - e.pageY * vel;
-
-    elem.style.transform = `translateX(${x}px) translateY(${y}px)`;
-  });
-};
+// window.onscroll = (e: Event) => {
+//   let elements = [...document.getElementsByClassName(targetClass)];
+//   elements.forEach((element) => {
+//     if (
+//       element.getBoundingClientRect().top < window.innerHeight - offsetTop &&
+//       element.getBoundingClientRect().bottom
+//     ) {
+//       element.classList.add("slideUp");
+//     } else {
+//       element.classList.remove("slideUp");
+//     }
+//   });
+// };
