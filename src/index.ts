5e48503c9bf9278ee64eb1890652eccff8877226
lnardon <lucaslnardon@gmail.com>
2021-04-23 00:57:43 -0300

    🚀 Update package for 2.0 release

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,31 @@
 export const useParallax = (
-  targetClass: string = "",
-  offsetTop: number = 0
+  containerClass: string,
+  targetClass: string = ""
 ) => {
   window.onload = () => {
-    // Adds mouse listener
-    document.addEventListener("mousemove", (e) => {
-      const objs: any = [...document.getElementsByClassName(`.${targetClass}`)];
+    // Containber Ref
+    const container: any = document.getElementsByClassName(containerClass)[0];
+
+    // Mousemove function
+    const moveFunction = (e: MouseEvent) => {
+      const objs: any = [...document.getElementsByClassName(targetClass)];
       objs.forEach((elem: HTMLElement) => {
         const vel = Number(elem.getAttribute("parallax-speed"));
-        const x = (window.innerWidth - e.pageX * vel) / 100;
-        const y = (window.innerHeight - e.pageY * vel) / 100;
+        const x = (container.style.width - e.pageX * vel) / 100;
+        const y = (container.style.height - e.pageY * vel) / 100;
 
         elem.style.transform = `translateX(${x}px) translateY(${y}px)`;
       });
+    };
+
+    // Add MouseEnter event
+    container.addEventListener("mouseenter", () => {
+      container.addEventListener("mousemove", moveFunction);
+    });
+
+    // Add MouseLeave event
+    container.addEventListener("mouseleave", () => {
+      container.removeEventListener("mousemove", moveFunction);
     });
   };
 };
-
-// window.onscroll = (e: Event) => {
-//   let elements = [...document.getElementsByClassName(targetClass)];
-//   elements.forEach((element) => {
-//     if (
-//       element.getBoundingClientRect().top < window.innerHeight - offsetTop &&
-//       element.getBoundingClientRect().bottom
-//     ) {
-//       element.classList.add("slideUp");
-//     } else {
-//       element.classList.remove("slideUp");
-//     }
-//   });
-// };
